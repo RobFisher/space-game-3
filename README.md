079a18168ba88b199fd3cc9c9ea21e6ab039681f
Rob Fisher <robfisher@gmail.com>
2026-06-11 23:05:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Space Game
+
+A Rust TUI game project organized as a monorepo workspace.
+
+## Development
+
+Enter the Nix development shell:
+
+```sh
+nix develop path:.
+```
+
+Build the workspace:
+
+```sh
+cargo build
+```
+
+Run the top-level game binary:
+
+```sh
+cargo run -p space-game
+```
---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Space game Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs systems (system:
+          f (import nixpkgs {
+            inherit system;
+          }));
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            cargo
+            rustc
+            rustfmt
+            clippy
+          ];
+        };
+      });
+    };
+}
